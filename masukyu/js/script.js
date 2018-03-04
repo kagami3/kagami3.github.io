fcafcddcc941069bab8850e30b42a3ef44effd48
kagami3 <32806052+kagami3@users.noreply.github.com>
2018-03-04 10:25:40 +0900

    0305

--- masukyu/js/script.js
+++ masukyu/js/script.js
@@ -1,7 +1,7 @@
 //DOMの読み込み
-$(function(){
+$(function() {
 
-  //グローバル変数
+    //グローバル変数
     var windowWidth = $(window).width();
     var windowHeight = $(window).height();
 
@@ -26,117 +26,122 @@ $(function(){
     // 現状の番号
     var currentIndex = -1;
     //時間を制御する変数
-    　var timer;
+    　
+    var timer;
 
     changeSlide(0);
 
     //画像を切り替える関数
-    function changeSlide(newIndex){
+    function changeSlide(newIndex) {
 
-      //現在の番号は飛ばすように（押せないように）
-        if(currentIndex === newIndex){
-          return;//ここで終わり
+        //現在の番号は飛ばすように（押せないように）
+        if (currentIndex === newIndex) {
+            return; //ここで終わり
         }
 
-      //一旦タイマーを停止
-      if(timer){
-        clearTimeout(timer);
-        timer = null;//timerの値を空にする
-      }
+        //一旦タイマーを停止
+        if (timer) {
+            clearTimeout(timer);
+            timer = null; //timerの値を空にする
+        }
 
-      if(currentIndex >= 0){
-        //現在表示している写真を1秒でopacity0にする
-        slideShow.find('li:eq('+currentIndex+')').animate({opacity:0},1000);
-      }
-      //次の写真(newindex)を1秒でopacity1にする
-      slideShow.find('li:eq('+newIndex+')').animate({opacity:1},1000);
-      currentIndex = newIndex;//自分の番号を次の番号に更新
+        if (currentIndex >= 0) {
+            //現在表示している写真を1秒でopacity0にする
+            slideShow.find('li:eq(' + currentIndex + ')').animate({
+                opacity: 0
+            }, 1000);
+        }
+        //次の写真(newindex)を1秒でopacity1にする
+        slideShow.find('li:eq(' + newIndex + ')').animate({
+            opacity: 1
+        }, 1000);
+        currentIndex = newIndex; //自分の番号を次の番号に更新
 
-      //ページングの指定
-      slidePaging.find('li').each(function(index){
-        $(this).removeClass('selected');
-        if(index == currentIndex){
-              $(this).addClass('selected');
-          }
-      });
-      //タイマー
-      timer = setTimeout(nextIndex, 4000);
+        //ページングの指定
+        slidePaging.find('li').each(function(index) {
+            $(this).removeClass('selected');
+            if (index == currentIndex) {
+                $(this).addClass('selected');
+            }
+        });
+        //タイマー
+        timer = setTimeout(nextIndex, 4000);
     }
 
     //番号を進める関数
-    function nextIndex(){
-      var newIndex = currentIndex + 1; //次の番号
-      if(newIndex >= slideLength){
-          newIndex = 0;
-      }
-      changeSlide(newIndex);
+    function nextIndex() {
+        var newIndex = currentIndex + 1; //次の番号
+        if (newIndex >= slideLength) {
+            newIndex = 0;
+        }
+        changeSlide(newIndex);
     }
 
-  //クリックイベント
-  slidePaging.find('li').each(function(index){
-      $(this).on('click',function(){
-          //liの押した番号をchangeSlideに渡す
-          changeSlide(index);
-      });
-  });
+    //クリックイベント
+    slidePaging.find('li').each(function(index) {
+        $(this).on('click', function() {
+            //liの押した番号をchangeSlideに渡す
+            changeSlide(index);
+        });
+    });
 
-  //スクロールイベント
-  $(window).on('scroll',function(){
+    //スクロールイベント
+    $(window).on('scroll', function() {
 
-      //上からのスクロール値
-      var dy = $(this).scrollTop();
-      //console.log('dy: ' + dy);
+        //上からのスクロール値
+        var dy = $(this).scrollTop();
+        //console.log('dy: ' + dy);
 
-      //---------------------------------------------
-      //Nav
-      //---------------------------------------------
+        //---------------------------------------------
+        //Nav
+        //---------------------------------------------
 
-      //PCのサイズのみ
-      if(windowWidth > 767){
-        //もしdyがnavContainer以上になったら
-        if(dy >= navContainerTop){
-            $('header nav').addClass('nav-fixed');
-        } else {
-            $('header nav').removeClass('nav-fixed');
+        //PCのサイズのみ
+        if (windowWidth > 767) {
+            //もしdyがnavContainer以上になったら
+            if (dy >= navContainerTop) {
+                $('header nav').addClass('nav-fixed');
+            } else {
+                $('header nav').removeClass('nav-fixed');
+            }
         }
-      }
 
-      //---------------------------------------------
-      //Scroll Fade
-      //---------------------------------------------
+        //---------------------------------------------
+        //Scroll Fade
+        //---------------------------------------------
 
-      if(dy >= worksContainerTop - windowHeight){
-        console.log('worksContainerTopだよ');
-        $('.works-container').find('section').addClass('fade-in');
-      }
+        if (dy >= worksContainerTop - windowHeight) {
+            console.log('worksContainerTopだよ');
+            $('.works-container').find('section').addClass('fade-in-up');
+        }
 
-      if(dy >= newsContainerTop - windowHeight){
-        $('.news-container').find('section').addClass('fade-in-up');
-      }
-  });
+        if (dy >= newsContainerTop - windowHeight) {
+            $('.news-container').find('section').addClass('fade-in-up');
+        }
+    });
 
 
 
     //windowをリサイズしたら
-    $(window).on('resize',function(){
-      console.log('リサイズ');
-      windowWidth = $(window).width();
+    $(window).on('resize', function() {
+        console.log('リサイズ');
+        windowWidth = $(window).width();
 
-      if(windowWidth > 767){
-        //PC
-      } else{
-        //Mobile
-      }
+        if (windowWidth > 767) {
+            //PC
+        } else {
+            //Mobile
+        }
 
     });
 
     //------------------------------
     //Responsive Nav
     //------------------------------
-      $('.mobile-menu').on('click',function(){
+    $('.mobile-menu').on('click', function() {
         $('header nav').addClass('mobile-menu-open').addClass('fade-in');
-      });
-      $('.mobile-close').on('click',function(){
+    });
+    $('.mobile-close').on('click', function() {
         $('header nav').removeClass('mobile-menu-open').removeClass('fade-in');
-      });
+    });
 });
